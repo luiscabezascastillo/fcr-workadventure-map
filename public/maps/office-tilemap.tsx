9ae8c51a73f3fd2537c63e2e88c42f5b89070335
luiscabezascastillo <81832047+luiscabezascastillo@users.noreply.github.com>
2025-12-17 08:31:12 -0300

    Delete public/maps/office-tilemap.tsx

--- public/maps/office-tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="office-tilemap" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="tilesets/office-tilemap.png" width="160" height="160"/>
-</tileset>
